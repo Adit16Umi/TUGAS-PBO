928fa522443e0cbc05eba695da727f0f7be72e76
Adit16Umi <adityaandi204@gmail.com>
2026-04-15 23:28:41 +0800

    Add files via upload

---
+++ Tugas3/Buah.java
@@ -0,0 +1,29 @@
+public class Buah {
+    private final String nama;
+    private final double harga;
+    private int stok;
+
+    // Konstruktor sesuai gambar (n:nama, h:harga, s:stok)
+    public Buah(String n, double h, int s) {
+        this.nama = n;
+        this.harga = h;
+        this.stok = s;
+    }
+
+    public String getInfo() {
+        return nama + "   Rp " + (int)harga + " (stok:" + stok + " kg)";
+    }
+
+    public double hitungTotal(int qty) {
+        return harga * qty;
+    }
+
+    public boolean tersedia(int qty) {
+        if (stok >= qty) {
+            stok -= qty; // Kurangi stok jika tersedia
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
